8b55bf30c32589189e8d21205b0ea92a03f1a58a
Brett Hazen <brett@basho.com>
2016-05-16 16:57:16 -0600

    Add support for rebar3

--- tools.mk
+++ tools.mk
@@ -1,6 +1,6 @@
 #  -------------------------------------------------------------------
 #
-#  Copyright (c) 2014 Basho Technologies, Inc.
+#  Copyright (c) 2014-2016 Basho Technologies, Inc.
 #
 #  This file is provided to you under the Apache License,
 #  Version 2.0 (the "License"); you may not use this file
@@ -24,7 +24,7 @@
 #  wholesale when a new version of tools.mk is released.
 #  -------------------------------------------------------------------
 
-REBAR ?= ./rebar
+REBAR ?= ./rebar3
 REVISION ?= $(shell git rev-parse --short HEAD)
 PROJECT ?= $(shell basename `find src -name "*.app.src"` .app.src)
 
